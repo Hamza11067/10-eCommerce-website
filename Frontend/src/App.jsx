683ad1ac95dfd70cbbf8e38989896210ea704550
Hamza11067 <hamzakhalid1067@gmail.com>
2024-04-25 11:12:17 +0500

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen bg-zinc-900 flex items-center justify-center">
+        <h1 className="text-3xl font-bold text-green-500">
+          Hello eCommerce website!!!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
