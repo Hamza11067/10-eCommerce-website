4398fab780fd43bcf1d86a71ad2605697fd17cc9
Hamza11067 <hamzakhalid1067@gmail.com>
2024-05-16 07:28:38 +0500

    popular products page completed

--- Frontend/src/Pages/Shop.jsx
+++ Frontend/src/Pages/Shop.jsx
@@ -1,9 +1,10 @@
-import { Hero } from "../components";
+import { Hero, Popular } from "../components";
 
 const Shop = () => {
   return (
     <>
       <Hero />
+      <Popular />
     </>
   );
 };
--- Frontend/src/components/index.js
+++ Frontend/src/components/index.js
@@ -1,7 +1,11 @@
-import Navbar from "./Navbar/Navbar";
 import Hero from "./Hero/Hero.jsx";
+import Item from "./Item/Item.jsx";
+import Navbar from "./Navbar/Navbar";
+import Popular from "./Popular/Popular.jsx";
 
 export {
+    Hero,
+    Item,
     Navbar,
-    Hero
+    Popular
 }
