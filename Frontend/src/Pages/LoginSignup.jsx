a6d9390c4b0c98f2a995a3fb586e3b23b1dcbb09
Hamza11067 <hamzakhalid1067@gmail.com>
2024-06-04 09:45:49 +0500

    working on login signup page

--- Frontend/src/Pages/LoginSignup.jsx
+++ Frontend/src/Pages/LoginSignup.jsx
@@ -1,7 +1,22 @@
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className="loginSignup">
+      <div className="loginSignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginSignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginSignup-login">Already have an account? <span>login here.</span></p>
+        <div className="loginSignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, I agree to the terms & conditions</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
